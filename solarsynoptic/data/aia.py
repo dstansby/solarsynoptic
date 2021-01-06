b8c841ac847ab4a66b364e211498476a927c6b63
David Stansby <dstansby@gmail.com>
2021-01-06 18:25:34 +0000

    Add NRT data to aia

--- solarsynoptic/data/aia.py
+++ solarsynoptic/data/aia.py
@@ -1,6 +1,13 @@
+from datetime import datetime, timedelta
+import pathlib
+
+import astropy.units as u
+from sunpy.map import Map
 from sunpy.net import attrs as a
 
 from . import helpers
+# TODO: change this to get the sunpy directory from sunpy
+map_dir = pathlib.Path('/Users/dstansby/sunpy/data')
 
 __all__ = ['aia_start_of_day_map']
 
@@ -15,5 +22,29 @@ def aia_start_of_day_map(dtime, wlen):
     wlen : astropy.units.Quantity
         Wavelength of interest.
     """
-    query = a.Instrument('AIA'), a.Wavelength(wlen)
-    return helpers.start_of_day_map(dtime, *query)
+    dtime = helpers.start_of_day(dtime)
+
+    # Download from JSOC if older than 14 days; otherwise directly
+    # get an NRT map
+    if (dtime < datetime.now() - timedelta(days=13)):
+        query = a.Instrument('AIA'), a.Wavelength(wlen)
+        return helpers.start_of_day_map(dtime, *query)
+    else:
+        import parfive
+        dl = parfive.Downloader(max_conn=1)
+        wlen = int(wlen.to_value(u.Angstrom))
+        url = (f"http://jsoc2.stanford.edu/data/aia/synoptic/nrt/"
+               f"{dtime.year}/{dtime.month:02}/{dtime.day:02}/"
+               f"H0000/AIA{dtime.year}{dtime.month:02}{dtime.day:02}_"
+               f"000000_0{wlen}.fits")
+        dl.enqueue_file(url, filename=map_path(dtime, wlen))
+        res = dl.download()
+        if len(res.errors):
+            print(res.errors)
+            raise RuntimeError('Download failed')
+        return Map(map_path(dtime, wlen))
+
+
+def map_path(dtime, wlen):
+    datestr = dtime.strftime('%Y%m%d')
+    return map_dir / f'aia_{wlen}_nrt_{datestr}.fits'
