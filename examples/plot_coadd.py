32bc7977249869ba9f27b9ca48f11b5b8bb26dd3
David Stansby <dstansby@gmail.com>
2021-01-07 20:16:20 +0000

    Fix coadding

--- examples/plot_coadd.py
+++ examples/plot_coadd.py
@@ -5,34 +5,68 @@ AIA Carrington map
 ###############################################################################
 # Import required functions
 from datetime import datetime, timedelta
+import numpy as np
 
 import astropy.units as u
 import matplotlib.pyplot as plt
+import sunpy.map
 
-from solarsynoptic.data import aia_start_of_day_map
+from solarsynoptic.data import stereo_start_of_day_map, aia_start_of_day_map
 from solarsynoptic.reprojection import reproject_carrington
-from solarsynoptic.coadd import coadd
+from solarsynoptic.coadd import coadd, long_weights
 
 ###############################################################################
 # Define the output of the shape map. This is number of pixels in latitude and
 # longitude.
 shape_out = [180, 360]
+
 ###############################################################################
-# Fetch a single AIA map. This will download the first map observed on the
-# given day.
-maps_in = []
-for i in range(24):
+# Download STEREO maps
+maps_in_stereo = []
+for i in range(16):
     d = datetime.now() - timedelta(days=i)
-    maps_in.append(aia_start_of_day_map(d, 193 * u.Angstrom))
+    smap = stereo_start_of_day_map(d)
+    # Normalise to AIA
+    data = (smap.data - 720.0) * 110 / 170
+    data[data < 0] = np.nan
+    smap = sunpy.map.Map((data, smap.meta))
+    maps_in_stereo.append(smap)
+
+###############################################################################
+# Download AIA maps
+maps_in_aia = []
+for i in range(18):
+    d = datetime.now() - timedelta(days=i)
+    maps_in_aia.append(aia_start_of_day_map(d, 193 * u.Angstrom))
+
+norm = maps_in_aia[0].plot_settings['norm']
+norm.vmin = 40
+norm.vmax = 5000
 
 ###############################################################################
 # Reproject the maps into a Carrington frame of reference
-maps_in = [reproject_carrington(map_in, shape_out, latitude_projection='CEA') for map_in in maps_in]
+maps_in_stereo = [reproject_carrington(map_in, shape_out) for map_in in maps_in_stereo]
+maps_in_aia = [reproject_carrington(map_in, shape_out) for map_in in maps_in_aia]
+
+
+def weight_function(smap):
+    weights = long_weights(30 * u.deg)(smap)
+    factor = (datetime.now() - smap.date.to_datetime()) / timedelta(days=1)
+    return weights / factor
 
 ###############################################################################
 # Add the maps together
-map_out = coadd(maps_in)
+map_out = coadd(maps_in_stereo, weight_function=weight_function)
 fig = plt.figure()
-map_out.plot()
+map_out.plot(cmap='sdoaia193', norm=norm)
+fig.savefig('figs/stereo.png')
 
-plt.show()
+map_out = coadd(maps_in_aia, weight_function=weight_function)
+fig = plt.figure()
+map_out.plot(cmap='sdoaia193', norm=norm)
+fig.savefig('figs/aia.png')
+
+map_out = coadd(maps_in_aia + maps_in_stereo, weight_function=weight_function)
+fig = plt.figure()
+map_out.plot(cmap='sdoaia193', norm=norm)
+fig.savefig('figs/combined.png')
