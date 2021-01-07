47ed2a95fcb5a4f9dde88330b7f3d6dcd758273e
David Stansby <dstansby@gmail.com>
2021-01-07 20:16:56 +0000

    Many stereo fixes

--- solarsynoptic/data/stereo.py
+++ solarsynoptic/data/stereo.py
@@ -1,7 +1,11 @@
+from datetime import timedelta
 import pathlib
 
+import astropy.units as u
 from parfive import Downloader
 from sunpy.map import Map
+from sunpy.net import attrs as a, Fido
+from sunpy.time import TimeRange
 
 from . import helpers
 # TODO: change this to get the sunpy directory from sunpy
@@ -17,30 +21,41 @@ def map_path(dtime):
 
 
 def stereo_beacon_start_of_day(dtime):
+    from sunpy.util.scraper import Scraper
     datestr = dtime.strftime('%Y%m%d')
-    # TODO: be more intelligent and scrape the filenames
-    for time in ['000530', '001615', '005530', '010530', '011530', '043530', ]:
-        url = ('https://stereo-ssc.nascom.nasa.gov/pub/beacon/ahead/secchi/img'
-               f'/euvi/{datestr}/{datestr}_{time}_n7euA.fts')
-        dl = Downloader()
+    scrp = Scraper('https://stereo-ssc.nascom.nasa.gov/pub/beacon/ahead/secchi/img'
+                   f'/euvi/{datestr}/%Y%m%d_%H%M%S_n7euA.fts')
+    urls = scrp.filelist(TimeRange(dtime, dtime + timedelta(days=1)))
+    urls.sort()
+    if not len(urls):
+        raise RuntimeError(f'No EUVI beacon map available for {dtime}')
+    dl = Downloader()
+    have_195 = False
+    for url in urls:
         dl.enqueue_file(url, path=map_path(dtime).parent)
         files = dl.download()
         if len(files.errors):
+            raise RuntimeError(f'EUVI download failed for {dtime}')
+        if Map(files[0]).wavelength != 195 * u.Angstrom:
             continue
-        pathlib.Path(files[0]).replace(map_path(dtime))
-        return
-    raise RuntimeError(f'No EUVI beacon map available for {dtime}')
+        else:
+            pathlib.Path(files[0]).replace(map_path(dtime))
+            have_195 = True
+            break
+    if not have_195:
+        raise RuntimeError('No 195 angstrom images')
+    return map_path(dtime)
 
 
 def download_start_of_day_map(dtime):
     dtime = helpers.start_of_day(dtime)
-    query = (a.Time(dtime, dtime + timedelta(days=1), dtime),
+    '''query = (a.Time(dtime, dtime + timedelta(days=1), dtime),
              a.Instrument('EUVI'))
     result = Fido.search(*query)
     try:
         mappath = Fido.fetch(result[0, 0])[0]
-    except IndexError as e:
-        download_beacon(dtime)
+    except IndexError as e:'''
+    mappath = stereo_beacon_start_of_day(dtime)
     mappath = pathlib.Path(mappath)
     mappath.replace(map_path(dtime))
 
