378e24310b45030733fa53b4ecca5b604438421f
David Stansby <dstansby@gmail.com>
2022-01-10 17:08:38 +0000

    Test CEA projection

--- solarsynoptic/reprojection/tests/test_reprojection.py
+++ solarsynoptic/reprojection/tests/test_reprojection.py
@@ -9,15 +9,17 @@ from solarsynoptic.data import aia_start_of_day_map
 from solarsynoptic.reprojection import reproject_carrington
 
 
+@pytest.mark.parametrize('latitude_projection', ['CEA', 'CAR'])
 @pytest.mark.parametrize('cache', [True, False])
-def test_reprojection(cache):
+def test_reprojection(cache, latitude_projection):
     smap = aia_start_of_day_map(Time.now(), 193 * u.Angstrom)
 
     shape_out = (180, 360)
-    car_map = reproject_carrington(smap, shape_out, cache=cache)
+    car_map = reproject_carrington(smap, shape_out, cache=cache,
+                                   latitude_projection=latitude_projection)
 
     assert isinstance(car_map, GenericMap)
     assert car_map.data.shape == shape_out
     ll_coord = car_map.pixel_to_world(*(-0.5, -0.5) * u.pix)
-    assert ll_coord.lon == Longitude(-180 * u.deg)
-    assert ll_coord.lat == -90 * u.deg
+    assert u.allclose(ll_coord.lon, Longitude(-180 * u.deg))
+    assert u.allclose(ll_coord.lat, -90 * u.deg)
