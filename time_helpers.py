07b541f9a5be4b36d160657b8683e9cd228dbe57
David Stansby <dstansby@gmail.com>
2022-01-24 15:15:07 +0000

    Remove old files

--- time_helpers.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-
-def start_of_day(dtime):
-    """
-    Return a datetime at time 00:00:00 on the given date.
-    """
-    return datetime(dtime.year, dtime.month, dtime.day)
